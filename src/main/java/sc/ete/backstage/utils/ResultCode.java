f5dc153cdd52693cbd6e1b2d249c9415cde0a03b
arvin <2295996982@qq.com>
2022-03-20 13:06:19 +0800

    [shixiangyu]feat:add other response code

--- src/main/java/sc/ete/backstage/utils/ResultCode.java
+++ src/main/java/sc/ete/backstage/utils/ResultCode.java
@@ -1,14 +1,18 @@
 package sc.ete.backstage.utils;
 /**
  * create by: Arvin
- * description: 20000 是成功，20001是访问失败
  * @params:
  * @return
  */
 public enum ResultCode {
     SUCCESS(200,"request success"),
     NOT_FOUND(404,"not found request url"),
-    ERROR(201,"bad request");
+    ERROR(500,"request error"),
+    CREATED(201,"created success"),
+    NO_CONTENT(204,"success and no response"),
+    BAD_REQUEST(400,"bad request");
+
+
     public Integer CODE;
     public String MSG;
     private ResultCode (Integer code,String msg) {
